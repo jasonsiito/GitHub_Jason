97256061726a8755e5aab7125587024657c162a9
Jason_l <jlrpr51@gmail.com>
2024-03-18 10:47:50 -0400

    Anadi carpetas para mis laboritorios en github.

---
+++ C#/Ejercicio_01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //codigo de la aplicasion
+        Console.WriteLine("Hol mundo");
+        
+    }
+
+
+
+}
---
+++ Java/proyecto01/Proyecto01.java
@@ -0,0 +1,8 @@
+package Java.proyecto01;
+
+public class Proyecto01 
+{
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!!!");
+    }
+}
